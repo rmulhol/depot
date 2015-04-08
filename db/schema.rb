6bb1c5e966138d1c093287f35cb1664b49a7a9c1
Robert Mulholand <robmulholand@gmail.com>
2015-04-08 14:47:09 -0500

    reset project to restart project with Rails 4 book

--- db/schema.rb
+++ db/schema.rb
@@ -11,28 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150115170301) do
-
-  create_table "carts", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "line_items", force: true do |t|
-    t.integer  "product_id"
-    t.integer  "cart_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "quantity",   default: 1
-  end
-
-  create_table "products", force: true do |t|
-    t.string   "title"
-    t.text     "description"
-    t.string   "image_url"
-    t.decimal  "price",       precision: 8, scale: 2
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
