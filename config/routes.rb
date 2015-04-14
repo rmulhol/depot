9b002fc021cd497a618e9a2c630acadce3652614
Robert Mulholand <robmulholand@gmail.com>
2015-04-14 12:39:06 -0500

    add i18n with support for Enlish and Spanish

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,25 @@
 Rails.application.routes.draw do
   get "admin" => 'admin#index'
+
   controller :sessions do 
     get    "login"  => :new
     post   "login"  => :create
     delete "logout" => :destroy
   end
 
-  resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
+  get "sessions/create"
+  get "sessions/destroy"
 
-  get 'store/index'
-  resources :products do 
+  resources :users
+
+  resources :products do
     get :who_bought, on: :member
   end
 
-  root 'store#index', as: 'store'
+  scope "(:locale)" do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store'
+  end
 end
